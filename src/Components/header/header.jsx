4876c7b59aa4dee8778c40112f8cbfa0ee0913d5
Mahliyo <gasandigital13@gmail.com>
2026-04-27 17:24:53 +0500

    header qismi

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,11 +1,11 @@
-import logo from ".src "
+import logo from "./Image/logo.svg";
 function Header (){
     return (
         <>
          <div className="header">
             <div className="container">
              <div className="header-container">
-                <a className="logo" href="#"><img src="" alt="rasm" /></a>
+                <a className="logo" href="#"><img src={logo} alt="logo" /></a>
              </div>
 
             </div>
