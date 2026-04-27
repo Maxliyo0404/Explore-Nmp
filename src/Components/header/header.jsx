301334e853eee0f8352772faaa0048fb1434f07e
Mahliyo <gasandigital13@gmail.com>
2026-04-27 14:06:06 +0500

    header container logo

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,10 +1,11 @@
+import logo from ".src "
 function Header (){
     return (
         <>
          <div className="header">
             <div className="container">
              <div className="header-container">
-                
+                <a className="logo" href="#"><img src="" alt="rasm" /></a>
              </div>
 
             </div>
