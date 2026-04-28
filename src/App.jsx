faa0622e2236b2c5a109a43fad3af37d65408370
Mahliyo <gasandigital13@gmail.com>
2026-04-28 08:37:09 +0500

    hero-css fnish

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header/Header"
 import Hero from "./Components/Hero/Hero";
+import SectionOne from "./Components/SectionOne/SectionOne";
 function App (){
   return (
     <>
        <Header/>
        <Hero/>
+       <SectionOne/>
     </>
   )
 }
