4960f796ffec88cfe484dc0c3a441a867b01af41
Mahliyo <gasandigital13@gmail.com>
2026-04-28 06:43:53 +0500

    Hero qismigq o'tdim

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,15 @@ import "./Hero.css"
 function Hero (){
     return (
         <>
-        
+        <div className="Hero">
+            <div className="container">
+                <div className="hero-wrapper">
+                    <div className="hero-left">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
