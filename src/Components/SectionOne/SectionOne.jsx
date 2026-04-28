aec0f9a74ec7e3ecc6d0e9eb9340caa76c5feb3c
Mahliyo <gasandigital13@gmail.com>
2026-04-28 08:44:29 +0500

    SectionOnega left o'tdim

--- src/Components/SectionOne/SectionOne.jsx
+++ src/Components/SectionOne/SectionOne.jsx
@@ -9,6 +9,11 @@ function SectionOne (){
                     <div className="one-left">
                         <img className="one-rasm" src={one_rasm} alt="rasm" />
                     </div>
+                    <div className="one-right">
+                        <h2 className="one-title">A new way to explore the world </h2>
+                         <p className="one-text">For decades travellers have reached for Lonely Planet books when looking to plan and execute their perfect trip, but now, they can also let Lonely Planet Experiences lead the way</p>
+                         <button className="one-btn">Learn more</button>
+                    </div>
                 </div>
             </div>
           </div>
