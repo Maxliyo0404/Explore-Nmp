a01b8054031b43f42a82e6914f4a0bcd4c39d8ed
Mahliyo <gasandigital13@gmail.com>
2026-04-28 07:00:44 +0500

    Hero qismigq o'tdim left

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,35 @@ function Hero (){
                 <div className="hero-wrapper">
                     <div className="hero-left">
                         <h2 className="hero-title">Explore and Travel</h2>
-                        
+                         <h3>holiday finder</h3>
+                         <div className="line"></div>
+                        <form className="form">
+                           
+                                <select className="select" >
+                                    <option >Location</option>
+                                    <option >Abuja</option>
+                                    <option >Nigeria</option>
+                                </select>
+                          
+                                <select className="select" >
+                                    <option >Activity</option>
+                                    <option >Swimming</option>
+                                    <option >Hiking</option>
+                                </select>
+                          
+                                <select className="select" >
+                                    <option >Grade</option>
+                                    <option >Economy</option>
+                                    <option >Standart</option>
+                                </select>
+                          
+                                <select className="select" >
+                                    <option >Location</option>
+                                    <option >Abuja</option>
+                                    <option >Nigeria</option>
+                                </select>
+                          
+                        </form>
                     </div>
                 </div>
             </div>
