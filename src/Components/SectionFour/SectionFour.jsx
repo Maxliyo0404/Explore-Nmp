d19b5535e5d159b2d5c496c73a8e1ca424b68e82
Mahliyo <gasandigital13@gmail.com>
2026-04-29 03:05:39 +0500

    sectionFour right

--- src/Components/SectionFour/SectionFour.jsx
+++ src/Components/SectionFour/SectionFour.jsx
@@ -1,5 +1,6 @@
 import "./SectionFour.css";
 import { HiStar } from "react-icons/hi";
+import four_rasm from "./image/four_rasm.svg";
 function SectionFour (){
     return (
         <>
@@ -19,6 +20,10 @@ function SectionFour (){
                         <p className="four-text">“Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”</p>
                         <h3 className="four-new">Edward Newgate</h3>
                         <h4 className="four-circel">Founder Circle</h4>
+                      </div>
+                      <div className="four-right">
+                        <img className="four-img" src={four_rasm} alt="rasm" />
+                        <span></span>
                       </div>
                     </div>
                 </div>
