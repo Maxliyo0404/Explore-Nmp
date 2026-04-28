91f07098e80c918370c384cded14ce57acbbe119
Mahliyo <gasandigital13@gmail.com>
2026-04-28 08:39:10 +0500

    SectionOnega o'tdim

--- src/Components/SectionOne/SectionOne.jsx
+++ src/Components/SectionOne/SectionOne.jsx
@@ -1,8 +1,17 @@
 import "./SectionOne.css";
+import
 function SectionOne (){
     return (
         <>
-          
+          <div className="sectionOne">
+            <div className="container">
+                <div className="sectionOne-wrapper">
+                    <div className="one-left">
+                        <img className="one-rasm" src={} alt="rasm" />
+                    </div>
+                </div>
+            </div>
+          </div>
         </>
     )
 }
