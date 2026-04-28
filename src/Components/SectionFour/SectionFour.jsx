b3115f7b4c918618c6a010f783da759dd5c25d94
Mahliyo <gasandigital13@gmail.com>
2026-04-29 03:11:52 +0500

    sectionFour right

--- src/Components/SectionFour/SectionFour.jsx
+++ src/Components/SectionFour/SectionFour.jsx
@@ -1,6 +1,9 @@
 import "./SectionFour.css";
 import { HiStar } from "react-icons/hi";
 import four_rasm from "./image/four_rasm.svg";
+import span1 from "./image/span1.svg";
+import span2 from "./image/span2.svg";
+import span3 from "./image/span3.svg";
 function SectionFour (){
     return (
         <>
@@ -23,7 +26,13 @@ function SectionFour (){
                       </div>
                       <div className="four-right">
                         <img className="four-img" src={four_rasm} alt="rasm" />
-                        <span></span>
+                        <span className="four-spans">
+                          <img className="span-rasm" src={span1} alt="rasm" />
+                          <span className="four-spane">
+                             <img className="span-rasm1" src={span2} alt="rasm" />
+                             <img className="span-rasm2" src={span3} alt="rasm" />
+                          </span>
+                        </span>
                       </div>
                     </div>
                 </div>
