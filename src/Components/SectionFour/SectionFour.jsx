200aeff5cd2e82e44c20f7a4827dfb76a455cc94
Mahliyo <gasandigital13@gmail.com>
2026-04-29 02:42:54 +0500

    sectionFour left

--- src/Components/SectionFour/SectionFour.jsx
+++ src/Components/SectionFour/SectionFour.jsx
@@ -7,7 +7,13 @@ function SectionFour (){
             <div className="container">
                 <div className="sectionFour-wrapper">
                     <h2 className="four-title">Testimonials</h2>
-                    
+                    <div className="four-cards">
+                      <div className="four-left">
+                        <div className="four-icons">
+                          
+                        </div>
+                      </div>
+                    </div>
                 </div>
             </div>
           </div>
