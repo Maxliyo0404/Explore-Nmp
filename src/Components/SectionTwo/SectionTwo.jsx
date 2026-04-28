e06406aa1ad815be8a66cb29c1e77693d1fe81ee
Mahliyo <gasandigital13@gmail.com>
2026-04-28 09:31:10 +0500

    sectionTwo title

--- src/Components/SectionTwo/SectionTwo.jsx
+++ src/Components/SectionTwo/SectionTwo.jsx
@@ -1,4 +1,7 @@
 import "./SectionTwo.css";
+import { IoLocationSharp } from "react-icons/io5";
+import { IoCalendarOutline } from "react-icons/io5";
+import { SlArrowRight } from "react-icons/sl";
 function SectionTwo (){
     return (
         <>
@@ -7,8 +10,17 @@ function SectionTwo (){
                 <div className="sectionTwo-wrapper">
                     <div className="two-titles">
                         <h2 className="two-title">Featured destinations</h2>
-                        <h4 className="two-text">View all<span className="span"> > </span></h4>
+                        <h4 className="two-text">View all<span className="span"><SlArrowRight /></span></h4>
                     </div>
+                    <ul className="two-list">
+                        <li className="two-item">
+                        <img className="two-rasm" src={} alt="" />
+                        <span className="two-span">
+                            <h4 className="span-title"></h4>
+                             <p className="span-text"></p>
+                        </span>
+                        </li>
+                    </ul>
                 </div>
             </div>
            </div>
