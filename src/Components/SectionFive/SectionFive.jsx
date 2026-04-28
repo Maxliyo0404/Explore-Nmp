f4f7386c0f899a15a3c0fc49b08a7406241d456b
Mahliyo <gasandigital13@gmail.com>
2026-04-29 04:55:27 +0500

    sectionFive qismi start

--- src/Components/SectionFive/SectionFive.jsx
+++ src/Components/SectionFive/SectionFive.jsx
@@ -1,12 +1,18 @@
 import "./SectionFive.css";
-
+import { IoLocationSharp } from "react-icons/io5";
+import { IoCalendarOutline } from "react-icons/io5";
+import { SlArrowRight } from "react-icons/sl";
 function SectionFive (){
     return (
         <>
           <div className="sectionFive">
             <div className="container">
                 <div className="sectionFive-wrapper">
-                    
+                      <div className="five-titles">
+                         <h2 className="five-title">Featured destinations</h2>
+                         <h4 className="five-text">View all <br /><span className="spans"><SlArrowRight /></span></h4>
+                    </div>
+                    <ul className=""></ul>
                 </div>
             </div>
           </div>
