98fdbf80e2fa26d2b539a94c4bec1bc934875c92
Mahliyo <gasandigital13@gmail.com>
2026-04-29 04:50:52 +0500

    sectionFour css berish cards finish

--- src/Components/SectionFive/SectionFive.jsx
+++ src/Components/SectionFive/SectionFive.jsx
@@ -3,7 +3,11 @@ import "./SectionFive.css";
 function SectionFive (){
     return (
         <>
-        
+          <div className="sectionFive">
+            <div className="container">
+                
+            </div>
+          </div>
         </>
     )
 }
