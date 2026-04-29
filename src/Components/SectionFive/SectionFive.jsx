d85590772deea20ee1750ac01f2b12f45fb5ed44
Mahliyo <gasandigital13@gmail.com>
2026-04-29 05:10:32 +0500

    sectionFive start

--- src/Components/SectionFive/SectionFive.jsx
+++ src/Components/SectionFive/SectionFive.jsx
@@ -2,6 +2,7 @@ import "./SectionFive.css";
 import { IoLocationSharp } from "react-icons/io5";
 import { IoCalendarOutline } from "react-icons/io5";
 import { SlArrowRight } from "react-icons/sl";
+import five_rasm1 from "./image/five_rasm1.svg";
 function SectionFive (){
     return (
         <>
@@ -14,9 +15,35 @@ function SectionFive (){
                     </div>
                     <ul className="five-list">
                         <li className="five-item">
+                            <img className="five-rasm" src={five_rasm1} alt="rasm" />
+                            <div className="five-titles">
+                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
+                                <p className="five-texte">‘Helaing holidays’ are on the rise tohelp maximise your health and happines...</p>
+                                <a className="link" href="#">Read more</a>
+                            </div>
+                        </li>
+                        <li className="five-item">
                             <img className="five-rasm" src={} alt="rasm" />
                             <div className="five-titles">
-                                
+                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
+                                <p className="five-texte"></p>
+                                <a className="link" href="#">Read more</a>
+                            </div>
+                        </li>
+                        <li className="five-item">
+                            <img className="five-rasm" src={} alt="rasm" />
+                            <div className="five-titles">
+                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
+                                <p className="five-texte"></p>
+                                <a className="link" href="#">Read more</a>
+                            </div>
+                        </li>
+                        <li className="five-item">
+                            <img className="five-rasm" src={} alt="rasm" />
+                            <div className="five-titles">
+                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
+                                <p className="five-texte"></p>
+                                <a className="link" href="#">Read more</a>
                             </div>
                         </li>
                     </ul>
