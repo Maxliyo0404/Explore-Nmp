4103e8338f2476b39a38b9478f7b51dbdd658baf
Mahliyo <gasandigital13@gmail.com>
2026-04-29 05:15:02 +0500

    sectionFive start

--- src/Components/SectionFive/SectionFive.jsx
+++ src/Components/SectionFive/SectionFive.jsx
@@ -3,6 +3,9 @@ import { IoLocationSharp } from "react-icons/io5";
 import { IoCalendarOutline } from "react-icons/io5";
 import { SlArrowRight } from "react-icons/sl";
 import five_rasm1 from "./image/five_rasm1.svg";
+import five_rasm2 from "./image/five_rasm2.svg";
+import five_rasm3 from "./image/five_rasm3.svg";
+import five_rasm4 from "./image/five_rasm4.svg";
 function SectionFive (){
     return (
         <>
@@ -23,25 +26,25 @@ function SectionFive (){
                             </div>
                         </li>
                         <li className="five-item">
-                            <img className="five-rasm" src={} alt="rasm" />
+                            <img className="five-rasm" src={five_rasm2} alt="rasm" />
                             <div className="five-titles">
-                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
-                                <p className="five-texte"></p>
+                                <h5 className="five-many">The best Kyoto restaurant to try Japanese food</h5>
+                                <p className="five-texte">From tofu to teahouses, here’s our guide to Kyoto’s best restaurants to visit...</p>
                                 <a className="link" href="#">Read more</a>
                             </div>
                         </li>
                         <li className="five-item">
-                            <img className="five-rasm" src={} alt="rasm" />
+                            <img className="five-rasm" src={five_rasm3} alt="rasm" />
                             <div className="five-titles">
-                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
-                                <p className="five-texte"></p>
+                                <h5 className="five-many">Skip Chichen Itza and head to this remote Yucatan</h5>
+                                <p className="five-texte">It’s remote and challenging to get, but braving the jungle and exploring these ruins without the...</p>
                                 <a className="link" href="#">Read more</a>
                             </div>
                         </li>
                         <li className="five-item">
-                            <img className="five-rasm" src={} alt="rasm" />
+                            <img className="five-rasm" src={five_rasm4} alt="rasm" />
                             <div className="five-titles">
-                                <h5 className="five-many">The many benefits of  taking a healing holiday</h5>
+                                <h5 className="five-many">Surf’s up at these beginner spots around the world</h5>
                                 <p className="five-texte"></p>
                                 <a className="link" href="#">Read more</a>
                             </div>
