8701bdbb4fa9838a49fcfcf797812723b6f5556b
Mahliyo <gasandigital13@gmail.com>
2026-04-29 05:47:21 +0500

    sectionFive start container css fnish

--- src/Components/SectionSix/SectionSix.jsx
+++ src/Components/SectionSix/SectionSix.jsx
@@ -1,8 +1,45 @@
 import "/.SectionSix.css";
+import six from "./image/six.svg"
 function SectionSix () {
     return (
         <>
-        
+         <div className="sectionSix">
+            <div className="container">
+                <div className="sectionSix-wrapper">
+                    <ul className="six-list">
+                        <li className="six-item">
+                         <img className="six-rasm" src={six} alt="" />
+                         <p className="six-text">Plan and book your perfect trip with expert advice, travel tips destination information from us</p>
+                         <a className="sixs" href="#">©2020 Thousand Sunny. All rights reserved</a>
+                        </li>
+                        <li className="six-item">
+                            <a className="six-link" href="#">Destinations</a>
+                            <a className="six-link" href="#">Africa  </a>
+                            <a className="six-link" href="#">Antarctica</a>
+                            <a className="six-link" href="#">Asia</a>
+                            <a className="six-link" href="#"> Europe </a>
+                            <a className="six-link" href="#">America</a>
+                        </li>
+                        <li className="six-item">
+                            <a className="six-link" href="#">Shop</a>
+                            <a className="six-link" href="#">Destination Guides  </a>
+                            <a className="six-link" href="#">Pictorial & Gifts</a>
+                            <a className="six-link" href="#">Special Offers </a>
+                            <a className="six-link" href="#">Delivery Times</a>
+                            <a className="six-link" href="#"> FAQs</a>
+                        </li>
+                        <li className="six-item">
+                            <a className="six-link" href="#"></a>
+                            <a className="six-link" href="#"></a>
+                            <a className="six-link" href="#"></a>
+                            <a className="six-link" href="#"></a>
+                            <a className="six-link" href="#"></a>
+                            <a className="six-link" href="#"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+         </div>
         </>
     )
 }
